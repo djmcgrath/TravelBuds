ffcaffdc0b67dca40e1cabd029b9e656a6d375d6
djmcgrath <djmcgrath95@gmail.com>
2023-12-12 13:29:19 -0700

    got the tabs for frontend

--- travel/app/(tabs)/_layout.tsx
+++ travel/app/(tabs)/_layout.tsx
@@ -1,55 +1,43 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable, useColorScheme } from 'react-native';
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import Colors from '../../constants/Colors'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
-import Colors from '../../constants/Colors';
-
-/**
- * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
- */
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+const Layout = () => {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
+    <Tabs screenOptions={{
+        tabBarActiveTintColor: Colors.primary,
+        tabBarLabelStyle: {
+            fontFamily: "mon-sb",
+        },
+    }}>
+        <Tabs.Screen name='maps' options={{
+            tabBarLabel: "Maps",
+            tabBarIcon: ({ color, size}) => 
+            <MaterialCommunityIcons name='map-marker-radius-outline' color={color} size={size}/>
         }}
-      />
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        />
+        <Tabs.Screen name='group' options={{
+            tabBarLabel: "Group Page",
+            tabBarIcon: ({ color, size}) => 
+            <MaterialCommunityIcons name='account-group' color={color} size={size}/>
         }}
-      />
+        />
+        <Tabs.Screen name='user_group' options={{
+            tabBarLabel: "Your Groups",
+            tabBarIcon: ({ color, size}) => 
+            <MaterialCommunityIcons name='account-group-outline' color={color} size={size}/>
+        }}
+        />
+        <Tabs.Screen name='profile' options={{
+            tabBarLabel: "Profile",
+            tabBarIcon: ({ color, size}) => 
+            <MaterialCommunityIcons name='account' color={color} size={size}/>
+        }}
+        />
     </Tabs>
-  );
+  )
 }
+
+export default Layout
---
+++ travel/app/(tabs)/user_group.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const user_group = () => {
+  return (
+    <View>
+      <Text>user_group</Text>
+    </View>
+  )
+}
+
+export default user_group
